648ebbc4eb31def90b84593c620d2f85f79f2da2
Matti Kantola <matti.kantola@helsinki.fi>
2022-10-26 19:13:00 +0300

    eka tehtävä purkissa, tulee pitkä kurssi

---
+++ weird_algorithm.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+int main(void){
+
+    long long int nnn;
+    std::cin >> nnn;
+        while (nnn>1){
+
+        std::cout << nnn << " ";
+
+        if (nnn%2 == 0){
+            nnn /= 2;
+        }
+        else{
+            nnn = 3*nnn + 1;
+        }
+        
+    }
+    std::cout << nnn << std::endl;
+    return 0;
+
+}
