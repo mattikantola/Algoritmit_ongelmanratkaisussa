371a6e41912baf0339400f6a79d3f64bae3f4d22
Matti Kantola <matti.kantola@helsinki.fi>
2023-03-30 13:14:12 +0300

    Toinen Josephus valmis

---
+++ second_josephus.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int,null_type,std::less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+
+int main(){
+
+    long long int upper_limit, skip;
+    indexed_set storage;
+
+    std::cin >> upper_limit >> skip;
+
+    for (long long int iii=1; iii<=upper_limit;iii++){
+        storage.insert(iii);
+    }
+
+    long long int iii = skip;
+    while (!storage.empty()){
+        iii = iii % storage.size();
+        auto x = *storage.find_by_order(iii);
+        if (x != 0){std::cout << x << " ";
+        storage.erase(x);}
+        iii += skip;
+    }
+
+    return 0;
+}
